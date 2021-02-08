c5304103bdae4a539aae2c3d189b72d9394dd2cc
user <chanyoung091@naver.com>
2021-02-08 13:14:54 +0900

    오키도키요 2월8일 자바

---
+++ java210208/src/com/base/Helloworld.java
@@ -0,0 +1,21 @@
+package com.base;
+
+public class Helloworld {
+   int age = 26; // 전역변수
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		Helloworld hw = new Helloworld();
+		hw.age = 31;
+		
+		System.out.println("당신의 나이는"+hw.age+"입니다.");
+		
+		
+		
+		
+
+	}
+
+}
